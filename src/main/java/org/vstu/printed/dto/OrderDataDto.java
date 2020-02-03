1db9d6c25b61598d95c9594d96a556a66ec3ac40
Nikita <jdmcorolla21@gmail.com>
2020-02-04 02:36:17 +0400

    Added required property for location field on OrderDataDto

--- src/main/java/org/vstu/printed/dto/OrderDataDto.java
+++ src/main/java/org/vstu/printed/dto/OrderDataDto.java
@@ -1,11 +1,13 @@
 package org.vstu.printed.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class OrderDataDto {
+  @JsonProperty(required = true)
   private double[] location;
   private int radius;
   private String receiveOption;
