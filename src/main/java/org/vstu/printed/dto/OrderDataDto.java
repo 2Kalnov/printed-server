fac3f1f599cd12308776f9ec14b8544c5ea3d945
2Kalnov <jdmcorolla21@gmail.com>
2021-03-14 14:51:50 +0300

    Made receiveOption to be required in OrderDataDto

--- src/main/java/org/vstu/printed/dto/OrderDataDto.java
+++ src/main/java/org/vstu/printed/dto/OrderDataDto.java
@@ -1,14 +1,17 @@
 package org.vstu.printed.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
 public class OrderDataDto {
-  @JsonProperty(required = true)
   private double[] location;
   private int radius;
+
+  @JsonProperty(required = true)
   private String receiveOption;
 }
